48b7884bd0985218c1fa084dbf6f3a4dd88d6867
Regina De Gasperis <degasperisr@gmail.com>
2021-02-22 22:21:20 -0300

    plan

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -9,7 +9,7 @@ function HomePage(){
             <p className={s.texto1}> ¡Qué presión! </p>
             <p className={s.texto2}> Pero sabes qué, ¡No desesperes!. Accedé gratuitamente a un formulario, contestá las preguntas y obtené un breve plan de comunicación digital. </p>
             <p className={s.texto3}>¡No te quedes afuera de internet!</p>
-            <div className={s.contBoton}><Link  type='button' to='/Plan'><button className={s.boton} >Quiero comunicar, quiero mi plan</button></Link></div>
+            <div className={s.contBoton}><Link  type='button' to='/quiero-comunicar/Plan'><button className={s.boton} >Quiero comunicar, quiero mi plan</button></Link></div>
             <p className={s.texto4}>¡No olvides! Para un mejor resultado, se requiere un plan a medida, para eso, asesorate con un profesional del área.</p>
         </div>
     )
