7f01893a1a8317fd4db594732df0d9d4bb48931b
Regina De Gasperis <degasperisr@gmail.com>
2021-09-23 18:50:38 -0300

    finalcambios

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import NavBar from "./component/NavBar";
+import Header from "./component/Home/Header";
+import HomePage from "./component/Home/Home";
+import SobreMi from "./component/SobreMi/SobreMi";
+import Contacto from "./component/Contacto/Contacto";
+import Plan from "./component/Plan/Plan";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/Plan" exact>
+          <Plan />
+        </Route>
+        <Route path="/SobreMi" exact>
+          <SobreMi />
+        </Route>
+        <Route path="/Contacto" exact>
+          <Contacto />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
-import imgs from './imgs/imgs';
-import s from './styles/NavBar.module.css'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import imgs from "./imgs/imgs";
+import s from "./styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
-
-
-function NavBar(){
-    return (
-        <div className={s.contenedor}>
-            <nav className={s.nav}>
-            <div className={s.logo} to='/'>
-                    <img className={s.comunicar} src={imgs.comunicar} alt="#QuieroComunicar"/>
-                    
-                </div>  
-            <div className={s.menu}>                    
-                <NavLink className={s.texto} to='/quiero-comunicar/'> Inicio </NavLink>                    
-                <NavLink className={s.texto} to="/quiero-comunicar/Plan/">Plan</NavLink>                      
-                <NavLink className={s.texto} to="/quiero-comunicar/Contacto/">Contacto</NavLink>
-                <NavLink className={s.texto} to="/quiero-comunicar/SobreMi/">Sobre Mi</NavLink>
-                </div>             
-                <div> <h1 className={s.barText}> Versión Beta </h1></div>
-            </nav>
+function NavBar() {
+  return (
+    <div className={s.contenedor}>
+      <nav className={s.nav}>
+        <div className={s.logo} to="/">
+          <img
+            className={s.comunicar}
+            src={imgs.comunicar}
+            alt="#QuieroComunicar"
+          />
         </div>
-    )
+        <div className={s.menu}>
+          <NavLink className={s.texto} to="/">
+            {" "}
+            Inicio{" "}
+          </NavLink>
+          <NavLink className={s.texto} to="/Plan/">
+            Plan
+          </NavLink>
+          <NavLink className={s.texto} to="/Contacto/">
+            Contacto
+          </NavLink>
+          <NavLink className={s.texto} to="/SobreMi/">
+            Sobre Mi
+          </NavLink>
+        </div>
+        <div>
+          <h1 className={s.barText}> Versión Beta </h1>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
